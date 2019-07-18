a183cd8def84dba6000e76a54e9dbe8ac468049d
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-18 21:42:39 +0900

    Add createCategory Mutation

---
+++ server/app/graphql/mutations/create_category.rb
@@ -0,0 +1,17 @@
+module Mutations
+  class CreateCategory < Mutations::BaseMutation
+    field :category, Types::CategoryType, null: true
+    field :result, Boolean, null: true
+
+    argument :name, String, required: false
+    argument :description, String, required: false
+
+    def resolve(**args)
+      category = Category.create(name: args[:name], description: args[:description] )
+      {
+        category: category,
+        result: category.errors.blank?
+      }
+    end
+  end
+end
--- server/app/graphql/types/mutation_type.rb
+++ server/app/graphql/types/mutation_type.rb
@@ -1,9 +1,5 @@
 module Types
   class MutationType < Types::BaseObject
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
+    field :createCategory, mutation: Mutations::CreateCategory
   end
 end
