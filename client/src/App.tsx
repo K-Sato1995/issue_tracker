18625b78ec2aa5725ec294cca591f52e6578007c
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-20 13:48:51 +0900

    Replace Front end with React in TypeScript

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "./logo.svg";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+};
+
+export default App;
