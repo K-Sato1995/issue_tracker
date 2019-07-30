68b334b4e2dc253c25ba46010e03f9312f65c13e
Katsuki <Katsuki_Sato0130@icloud.com>
2019-07-30 19:16:41 +0900

    Fix spec

--- server/spec/graphql/mutations/create_project_spec.rb
+++ server/spec/graphql/mutations/create_project_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Mutations::Projects::CreateProject do
-  subject(:mutation) { described_class.new(object: nil, context: nil) }
+  let(:user) { create(:user) }
+  subject(:mutation) { described_class.new(object: nil, context: {current_user: user}) }
   let(:attributes) { attributes_for(:project, name: Faker::App.name) }
 
   describe '#resolve' do
     subject { mutation.resolve(attributes) }
     it 'creates a project' do
-      expect( subject[:project].name).to eq attributes[:name]
+      expect(subject[:project].name).to eq attributes[:name]
     end
   end
 end
