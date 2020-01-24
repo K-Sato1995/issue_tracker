068cd1b55bacde5a6b93a79ebf0ead7485127768
Katsuki <Katsuki_Sato0130@icloud.com>
2020-01-24 20:44:47 +0900

    Add project query to projectDetail

--- client/src/components/projectDetail/index.tsx
+++ client/src/components/projectDetail/index.tsx
@@ -1,11 +1,39 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { GetProject, GetProjectVariables } from "__types__/GetProject";
+import { useQuery } from "@apollo/react-hooks";
+import { loader } from "graphql.macro";
 
-const index = () => {
+const GET_PROJECT = loader("src/graphql/queries/getProject.graphql");
+
+const ProjectDetail = () => {
+  const { id } = useParams();
+  if (!id) {
+    throw new Error();
+  }
+  const { data, error, loading } = useQuery<GetProject, GetProjectVariables>(
+    GET_PROJECT,
+    {
+      variables: { id }
+    }
+  );
+
+  if (error) {
+    return <>Error! {error.message}</>;
+  }
+
+  if (!data || loading) {
+    return <>Loading...</>;
+  }
+
+  const project = data.project;
+
   return (
     <div>
-      <h1>ProjectDetail</h1>
+      <h1>{project.name}</h1>
+      <h2>Issues</h2>
     </div>
   );
 };
 
-export default index;
+export default ProjectDetail;
